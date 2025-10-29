6c43450b956f70d05d23d775ca6bd59749c30424
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2025-10-29 00:39:43 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,12 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'cff0c6ed1f974f20c4fdbbe2aa03c1b8'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+#Vai verificar se existe a variavel de ambiente se não existir vai usar o banco de teste
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:OpUyYkGrEflzOcrNqkveRorDMpNAzQeS@hopper.proxy.rlwy.net:24219/railway'
+
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
